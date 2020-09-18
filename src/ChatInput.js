de725fed95e0b2d5ed2293fe1834d837b5f52294
Kelina Hu <kelinahu369@gmail.com>
2020-09-18 17:13:45 -0400

    fixes #2 Add the ability to chat

--- src/ChatInput.js
+++ src/ChatInput.js
@@ -1,27 +1,42 @@
 import React, { useState } from 'react'
 import './ChatInput.css'
-import db from './firebase';
-import firebase from 'firebase'
 import { useStateValue } from './StateProvider';
+
+var socket = new WebSocket("ws://localhost:8080/myapp");
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
 
 function ChatInput({ channelName, channelId }) {
     const [input, setInput] = useState('');
     const [{ user }] = useStateValue();
+
+
     const sendMessage = e => {
         e.preventDefault();
-
-        if(channelId) {
-            db.collection('rooms').doc(channelId).collection({
-            messages: input,
-            timestamp: firebase.firestore.FieldValue.server.timestamp,
-            user: user.displayName,
-            userimage: user.photoURL,
-
-        })
+        if (socket.readyState == WebSocket.OPEN) {
+            socket.send(input);
+        } else {
+            alert("The socket is not open.");
         }
+        return false;
+        
+        // if(channelId) {
+        //     db.collection('rooms').doc(channelId).collection({
+        //     messages: input,
+        //     timestamp: firebase.firestore.FieldValue.server.timestamp,
+        //     user: user.displayName,
+        //     userimage: user.photoURL,
+
+        // })
+        //}
     };
     return (
         <div className='chatInput'>
+            <div id="chat" >
+                
+            </div>
+
             <form>
                 <input 
                 value={input}
